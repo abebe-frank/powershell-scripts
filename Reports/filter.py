70e5657b9c37ad2e704ca409db9cf0673735bf8c
abebe-frank <60877612+abebe-frank@users.noreply.github.com>
2024-01-25 15:47:05 +0300

    Update filter.py

--- Reports/filter.py
+++ Reports/filter.py
@@ -11,6 +11,36 @@ users = pd.read_csv(userdata_path)
 # Load the CSV file into a DataFrame
 df = pd.read_csv(mailbox_path)
 
+def convert_to_bytes(size_str):
+    # Extract numeric values and unit using regular expression
+    match = re.match(r'([\d.]+)\s*([KMGTPEZY]?B)', size_str)
+    
+    if match:
+        value, unit = match.groups()
+        value = float(value.replace(',', ''))
+        
+        # Convert to bytes
+        if unit == 'KB':
+            value *= 1024
+        elif unit == 'MB':
+            value *= 1024 ** 2
+        elif unit == 'GB':
+            value *= 1024 ** 3
+        elif unit == 'TB':
+            value *= 1024 ** 4
+        elif unit == 'PB':
+            value *= 1024 ** 5
+        elif unit == 'EB':
+            value *= 1024 ** 6
+        elif unit == 'ZB':
+            value *= 1024 ** 7
+        elif unit == 'YB':
+            value *= 1024 ** 8
+
+        return int(value)
+    else:
+        return None
+
 # Iterate through unique user principal names
 for user_principal_name in users['User principal name'].unique():
     # Use boolean indexing to get the 'Licenses' value for the current user principal name
@@ -24,6 +54,14 @@ for user_principal_name in users['User principal name'].unique():
 
     # Add the 'Licenses' column with the obtained value
     filtered_df['Licenses'] = licenses_value
+
+    #Apply the conversion function to the 'TotalItemSize' column
+    used_size = filtered_df['TotalItemSize'].apply(convert_to_bytes)
+    alocatedsize = filtered_df['ProhibitSendReceiveQuota-In-MB'].apply(convert_to_bytes)
+    perc_used_size = round((used_size * 100 / alocatedsize),2)
+
+    filtered_df['Percentage Used'] = perc_used_size
+
 
     # Save the filtered DataFrame to a CSV file
     filtered_df.to_csv('filtered_mailboxsize.csv', mode='a', header=not os.path.exists('filtered_mailboxsize.csv'), index=False)
