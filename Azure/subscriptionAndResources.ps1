f03b56a9a54965b5447494a0666797227aeb0bc5
Frank Abebe <60877612+abebe-frank@users.noreply.github.com>
2024-06-01 12:20:10 +0300

    Create subscriptionAndResources.ps1

---
+++ Azure/subscriptionAndResources.ps1
@@ -0,0 +1,22 @@
+# Define the output file path
+$outputFile = ""
+ 
+# Initialize an array to store the data
+$data = @()
+ 
+# Retrieve all Azure subscriptions
+Get-AzSubscription | ForEach-Object {
+    $subscriptionName = $_.Name
+    Set-AzContext -SubscriptionId $_.SubscriptionId
+ 
+    # Retrieve all resource groups for the current subscription
+    (Get-AzResourceGroup).ResourceGroupName | ForEach-Object {
+        $data += [PSCustomObject] @{
+            Subscription   = $subscriptionName
+            ResourceGroup = $_
+        }
+    }
+}
+ 
+# Export the collected data to a CSV file
+$data | Export-Csv -Path $outputFile -NoTypeInformation
