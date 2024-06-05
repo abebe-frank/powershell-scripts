03c0eb724187ac06e440c521f55f9f7c2dbe606d
Frank Abebe <60877612+abebe-frank@users.noreply.github.com>
2024-06-05 08:46:13 +0300

    Update Archive.ps1

--- Archive.ps1
+++ Archive.ps1
@@ -1,1 +1,7 @@
-Start-ManagedFolderAssistant -Identity "email"
+New-RetentionPolicyTag "3 Months Archive" –Type All –RetentionEnabled $true –AgeLimitForRetention 90 –RetentionAction MoveToArchive
+
+New-RetentionPolicy "3 Months Archive" –RetentionPolicyTagLinks "3 Months Archive"
+
+Set-Mailbox "user@email.com" -RetentionPolicy "3 Months Archive"
+
+Start-ManagedFolderAssistant –Identity "user@email.com"
