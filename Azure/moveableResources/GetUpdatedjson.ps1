c6b978c9f8cdf5f1151e1c19244500ce3809b16b
Frank Abebe <60877612+abebe-frank@users.noreply.github.com>
2024-08-21 12:00:56 +0300

    Add files via upload

---
+++ Azure/moveableResources/GetUpdatedjson.ps1
@@ -0,0 +1,44 @@
+﻿# Author: @FrankAbebe
+# Description: Fetches the list of non-movable resources from the Azure docs and saves them to a JSON file
+# Output: NonMovableResources.json
+# Notes: This script is provided as a convenience for updating the list of non-movable resources.
+# Dependencies: None (uses Invoke-WebRequest and built-in string functions)
+# Date: 2024-08-21
+# Version: 1.1.0
+
+$url = 'https://raw.githubusercontent.com/MicrosoftDocs/azure-docs/main/articles/azure-resource-manager/management/move-support-resources.md'
+$response = Invoke-WebRequest -Uri $url
+
+$nonMovableResourceTypes = @()
+
+$servicePattern = '^##\s+(.*?)$'
+$serviceMatches = [regex]::Matches($response.Content, $servicePattern, 'IgnoreCase, Multiline')
+
+$rowPattern = '\|\s*(.*?)\s*\|(.*?)\s*\|(.*?)\s*\|(.*?)\s*\|'
+$rowMatches = [regex]::Matches($response.Content, $rowPattern, 'IgnoreCase, Multiline')
+
+$serviceIndex = 0
+
+foreach ($rowMatch in $rowMatches) {
+    $rowPos = $rowMatch.Index
+
+    while ($serviceIndex -lt $serviceMatches.Count - 1 -and $rowPos -gt $serviceMatches[$serviceIndex + 1].Index) {
+        $serviceIndex++
+    }
+
+    $fullResourceType = "$($serviceMatches[$serviceIndex].Groups[1].Value)/$($rowMatch.Groups[1].Value)"
+    $fullResourceType = $fullResourceType -replace '>', '/' -replace '\|', '' -replace '\s', '' -replace '//', '/'
+
+    $resourceGroupMove = if ($rowMatch.Groups[2].Value -like '*Yes*') { 'Yes' } else { 'No' }
+    $subscriptionMove = if ($rowMatch.Groups[3].Value -like '*Yes*') { 'Yes' } else { 'No' }
+    $regionMove = if ($rowMatch.Groups[4].Value -like '*Yes*') { 'Yes' } else { 'No' }
+
+    $nonMovableResourceTypes += [PSCustomObject]@{
+        ResourceType   = $fullResourceType.Trim()
+        ResourceGroup  = $resourceGroupMove
+        Subscription   = $subscriptionMove
+        Region         = $regionMove
+    }
+}
+
+$nonMovableResourceTypes | ConvertTo-Json | Out-File -FilePath "NonMovableResources2.json"
